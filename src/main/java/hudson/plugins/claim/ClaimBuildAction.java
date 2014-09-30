10bef02b8043aaa21b86427b89e671449b2abbcb
Arran <demonicdioxide@gmail.com>
2014-09-30 19:54:33 +0100

    Copied across mohammad's "fix" for claim's posting to flowdock.

--- src/main/java/hudson/plugins/claim/ClaimBuildAction.java
+++ src/main/java/hudson/plugins/claim/ClaimBuildAction.java
@@ -1,8 +1,15 @@
 package hudson.plugins.claim;
 
-import java.io.ObjectStreamException;
-
 import hudson.model.Run;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.io.ObjectStreamException;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 
 public class ClaimBuildAction extends AbstractClaimBuildAction<Run> {
 
@@ -31,8 +38,128 @@ public class ClaimBuildAction extends AbstractClaimBuildAction<Run> {
     }
 
     @Override
-    String getUrl() {
-        return owner.getUrl();
+    public void claim(String claimedBy, String reason, boolean sticky) {
+        super.claim(claimedBy, reason, sticky);
+        try {
+            sendFlowdockPost(claimedBy, reason);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * This method is used to send a HTTP POST request to the flowdock API in
+     * order to add a chat message to a flow when a failing build was claimed.
+     * 
+     * @param claimedBy
+     *            The name of the user that claimed the build
+     * @param reason
+     *            The reason the user specified for claiming the failed build.
+     * @throws Exception
+     *             If an exception is encountered while calling the flowdock
+     *             REST API.
+     */
+    private void sendFlowdockPost(String claimedBy, String reason)
+            throws Exception {
+        String runNumber = "";
+        String jobName = "";
+        String jenkinsUrl = "";
+        if (owner != null) {
+            // replace the hash sign so that flowdock does not mistake this for
+            // a tag
+            runNumber = owner.getDisplayName().replace("#", "No. ");
+            jobName = owner.getParent().getDisplayName();
+            jenkinsUrl = owner.getAbsoluteUrl();
+        }
+
+        // This StringBuffer contains the message that will be sent to the
+        // flowdock chat
+        StringBuffer chatMessage = new StringBuffer();
+        chatMessage.append("A failing build was just claimed!\r\n");
+        chatMessage.append("Job: ").append(jobName).append(".\r\n");
+        chatMessage.append("Run: ").append(runNumber).append(".\r\n");
+        chatMessage.append("Claimed by: ").append(claimedBy).append(".\r\n");
+        chatMessage.append("Reason: ").append(reason).append(".\r\n");
+        chatMessage.append("Jenkins URL: ").append(jenkinsUrl);
+
+        // These are the tags for the chat message
+        String tags = "#claim";
+
+        // The username that will be shown in the chat
+        String externalUserName = "jenkins";
+
+        // The post data for the request containing url-encoded values
+        StringBuffer postData = new StringBuffer();
+        postData.append("content=").append(urlEncode(chatMessage.toString()));
+        postData.append("&tags=").append(urlEncode(tags));
+        postData.append("&external_user_name=").append(
+                urlEncode(externalUserName));
+
+        // flow token of a flow
+        String flowToken = "7170352b7bfbc0d71f6964f15903d986";
+
+        // prepare the HTTP request
+        URL url;
+        HttpURLConnection connection = null;
+
+        String flowdockUrl = "https://api.flowdock.com/messages/chat/"
+                + flowToken;
+        url = new URL(flowdockUrl);
+
+        connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Content-Type",
+                "application/x-www-form-urlencoded");
+        connection.setRequestProperty("Content-Length",
+                String.valueOf(postData.toString().getBytes().length));
+        connection.setUseCaches(false);
+        connection.setDoInput(true);
+        connection.setDoOutput(true);
+
+        // send the POST request
+        DataOutputStream wr = new DataOutputStream(connection.getOutputStream());
+        if(postData.toString().contains("release") || postData.toString().contains("develop") || postData.toString().contains("master")){
+        wr.writeBytes(postData.toString());
+        wr.flush();
+        wr.close();
+        }
+
+        // handle the response code
+        if (connection.getResponseCode() != 200) {
+            StringBuffer responseContent = new StringBuffer();
+            try {
+                BufferedReader in = new BufferedReader(new InputStreamReader(
+                        connection.getErrorStream()));
+                String responseLine;
+                while ((responseLine = in.readLine()) != null) {
+                    responseContent.append(responseLine);
+                }
+                in.close();
+            } catch (Exception ex) {
+                throw new Exception(
+                        "Flowdock returned an error response with status "
+                                + connection.getResponseCode() + " "
+                                + connection.getResponseMessage() + ", "
+                                + responseContent.toString() + "\n\nURL: "
+                                + flowdockUrl);
+            }
+        }
+
+    }
+
+    /**
+     * URL-encode a String object so that it is safe to be POSTed.
+     * 
+     * @param s
+     *            The String to be encoded.
+     * @return The URL-encoded String
+     * @throws UnsupportedEncodingException
+     *             If the encoding is not supported.
+     */
+    private String urlEncode(final String s)
+            throws UnsupportedEncodingException {
+        return URLEncoder.encode(s, "UTF-8");
     }
 
 }
